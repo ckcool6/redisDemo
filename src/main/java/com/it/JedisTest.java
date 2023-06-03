27a60d861abe972dde1ce2116c6bcdf9e9356634
rzdtjb <rzdtjb@gmail.com>
2023-06-04 02:17:57 +0800

    first

--- src/main/java/com/it/JedisTest.java
+++ src/main/java/com/it/JedisTest.java
@@ -1,7 +1,16 @@
 package com.it;
 
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
 public class JedisTest {
     public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.11.130", 6379);
+        jedis.lpush("list1","a","b");
 
+        Set<String> keys = jedis.keys("*");
+        System.out.println(keys);
+        jedis.close();
     }
 }
